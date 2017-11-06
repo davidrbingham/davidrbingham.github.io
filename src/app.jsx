023d7530ef1bf3b50359425f581390ddc48f481b
Bingham <dbinh@allstate.com>
2017-11-06 16:09:33 +0000

    Adding bones

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'styles/index.scss';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>It Works on github pages</h1>
+      </div>
+    )
+  }
+}
